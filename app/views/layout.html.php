1e6db6b33a16b440f745729be823a89cd5e5f785
NathanMBP <nathanstaylor1@gmail.com>
2015-07-16 11:23:14 +1000

    add favicon

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -7,15 +7,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" user-scalable="no" />
 
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link rel="stylesheet/less" type="text/css" href='<?php echo site_url() ?>assets/css/style.less'/>
 
 	<link rel="shortcut icon" href="<?php echo site_url() ?>assets/media/favicon/favicon.ico" type="image/x-icon"/>
 
 
     <script type='text/javascript' src='<?php echo site_url() ?>assets/js/less.min.js'></script>
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script type='text/javascript' src='<?php echo site_url() ?>assets/js/script.js'></script>
 </head>
 <body>
